a060516e319893991d4bad4e7d4af100d012a060
Zhenguo Zhang <zhangz.sci@gmail.com>
2021-09-14 23:39:16 +0000

    updated to include more features

--- annotate_all.sh
+++ annotate_all.sh
@@ -4,6 +4,9 @@ set -e
 
 if [[ $# -lt 1 ]]; then
     echo "Usage: $0 <region-file> <out-file> [<pos-only>]"
+    echo "Add annotation info: gene, promoter, exon, intron, cgi"
+    echo "<pos-only>: if provided, meaning <region-file> contains"
+    echo "single-column position, rather than two-column bed file"
     exit 1;
 fi
 
@@ -17,15 +20,21 @@ if [[ "$posOnly" ]]; then
 fi
 
 echo "Adding annotation: gene"
-./annotate_regions.R --out-file tmp1.$$.tsv.gz --feat-name gene \
-$posCol $inFile genes.bed.gz
+./annotate_regions.R --out-file tmpOut.$$.tsv.gz --feat-name gene \
+$posCol $inFile genes.bed.gz && mv tmpOut.$$.tsv.gz tmpIn.$$.tsv.gz
+echo "Adding annotation: promoter"
+./annotate_regions.R --out-file tmpOut.$$.tsv.gz --feat-name promoter \
+$posCol tmpIn.$$.tsv.gz promoters.bed.gz && mv tmpOut.$$.tsv.gz tmpIn.$$.tsv.gz
 echo "Adding annotation: exon"
-./annotate_regions.R --out-file tmp2.$$.tsv.gz --feat-name exon \
-$posCol tmp1.$$.tsv.gz exons.bed.gz
+./annotate_regions.R --out-file tmpOut.$$.tsv.gz --feat-name exon \
+$posCol tmpIn.$$.tsv.gz exons.bed.gz && mv tmpOut.$$.tsv.gz tmpIn.$$.tsv.gz
 echo "Adding annotation: intron"
-./annotate_regions.R --out-file $outFile --feat-name intron \
-    $posCol tmp2.$$.tsv.gz introns.bed.gz && \
-    rm tmp1.$$.tsv.gz tmp2.$$.tsv.gz
+./annotate_regions.R --out-file tmpOut.$$.tsv.gz --feat-name intron \
+$posCol tmpIn.$$.tsv.gz introns.bed.gz && mv tmpOut.$$.tsv.gz tmpIn.$$.tsv.gz
+echo "Adding annotation: CGI"
+./annotate_regions.R --out-file $outFile --feat-name CGI \
+    $posCol tmpIn.$$.tsv.gz CGIs.bed.gz && \
+    rm tmpIn.$$.tsv.gz
 
 echo "Job done"
 
